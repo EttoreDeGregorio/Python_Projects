c9db048f4379d8187a75ffbabc168e7ff55b8837
EttoreDeGregorio <ettoredegr@libero.it>
2018-11-16 10:48:16 +0100

    Added finished version of location_weather.py

--- location_weather.py
+++ location_weather.py
@@ -1,16 +1,37 @@
-#Script che stampa il meteo in base alla location
+#Script che stampa le previsioni di fino a 10 giorni in base alla location
 
-from weather import Weather, Unit 		#weather-api
-import subprocess as sp 				#per comandi di sistema
+from weather import Weather, Unit #weather-api
+import subprocess as sp #per comandi di sistema
 
-sp.call('clear',shell=True)
+sp.call('clear', shell = True)
 
 weather = Weather(unit = Unit.CELSIUS)
 
-location = weather.lookup_by_location('milan')
-condition = location.condition
+luogo = weather.lookup_by_location('milan')#cmabiare in base alla città
+previsioni = luogo.forecast
 
-print(condition.text)
+risp = " " 
 
+for forecast in previsioni:
+
+	print "\n--------\n"
+
+	print "Data: ", forecast.date
+	print "Condizioni: ", forecast.text
+	print "\n- Temperature -"
+	print "MAX:", forecast.high, "C ---  MIN:", forecast.low, "C"
+
+	print "\n--------\n"
+
+	print"\n\n\n\n Voui vedere le previsioni del giono seguente? (y/n)"
+	risp = raw_input()
+
+	if risp == "n" or risp == "N":
+		break
+
+	else:
+		sp.call('clear', shell = True)
+
+print "Premi INVIO per uscire..."
 aspetta = raw_input()
-sp.call('clear',shell=True)
+sp.call('clear', shell = True)
